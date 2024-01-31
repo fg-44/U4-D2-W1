40d5bde897bf2522112295c7de271abd984afc73
f.guglielmucci <148483575+fg-44@users.noreply.github.com>
2024-01-31 16:41:38 +0100

    Add files via upload

---
+++ U3-W1-D3/ContoCorrente.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Bank
+{
+    class ContoCorrente
+{
+        private string Name { get; set; }
+        private int Giacenza { get; set; }
+        private int Saldo { get; set; }
+        public Dictionary<string, object> Properties { get; set; }
+
+        public ContoCorrente(string name, int giacenza)
+    {
+        this.Name = name;
+        this.Giacenza = giacenza;
+        this.Saldo = giacenza; 
+        this.Properties = new Dictionary<string, object>();
+    }
+
+
+        // METODO VERSAMENT
+        public void Versamento(int importo)
+    {
+        this.Saldo += importo;
+    }
+        // METODO PRELIEVO
+    
+    public void Prelievo(int importo)
+    {
+        if (this.Saldo < importo)
+        {
+            Console.WriteLine("Insufficienza fondi per prelievo");
+        }
+        else
+        {
+            this.Saldo -= importo;
+        }
+
+
+    }
+       
+
+        // MEDOTO ARCHIVIA PROPRIETA ACCOUNT
+
+        public void Archivia(Dictionary<string, object> properties)
+    {
+        this.Properties = properties;
+    }
+        //GET NAME
+    public string GetName()
+    {
+        return Name;
+    }
+        //GET SALDO
+    public int GetSaldo()
+    {
+        return Saldo;
+    }
+        // PROPRIETA
+
+    public Dictionary<string, object> GetProperties()
+    {
+        return Properties;
+    }
+}
+
+
+}
+
---
+++ U3-W1-D3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Bank;
+
+namespace Bank
+{  
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            ContoCorrente conto1 = new ContoCorrente("Giangiggi", 1000);
+
+            conto1.Versamento(importo: 1000);
+            conto1.Prelievo(importo: 750);
+            conto1.Archivia(new Dictionary<string, object>() { { "giacenza", 1200 }, { "titolo_di_credito", "Credit" } });
+
+            Console.WriteLine(conto1.GetName());
+            Console.WriteLine(conto1.GetSaldo());
+            Console.WriteLine(conto1.GetProperties().Count);
+        }
+       
+    }
+
+
+}
